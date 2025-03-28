9eb939c40e130bb101bde14ffcfc01ccc46c9095
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-03-28 10:54:43 +0100

    interactive part

---
+++ src/app/components/interactive-part/interactive-part.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interactive-part',
+  imports: [],
+  templateUrl: './interactive-part.component.html',
+  styleUrl: './interactive-part.component.css'
+})
+export class InteractivePartComponent { }
