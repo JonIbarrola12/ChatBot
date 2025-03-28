2ed29467c3185acd096e1c1f33564dd012f4a2b4
Beñat Barbosa <binatbarzabal02@gmail.com>
2025-03-28 10:18:10 +0100

    ts navegator

--- src/app/components/navegator/navegator.component.ts
+++ src/app/components/navegator/navegator.component.ts
@@ -1,2 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chatbot-navegator',
+  templateUrl: 'navegator.component.html'
+})
+
+export class ChatbotNavegator{
 
 
+
+}
