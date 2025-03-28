ee211b54419f9a69d2ebcdaa6c3386428920ffbc
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-03-28 09:32:14 +0100

    Añadir el componente de la barra lateral y crear el archivo environment

---
+++ src/app/components/navegator/navegator.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navegator',
+  imports: [],
+  templateUrl: './navegator.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavegatorComponent { }
