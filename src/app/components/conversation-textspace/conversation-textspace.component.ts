e47b2da6ff5a8340911c23ea6c2b87528c4e85c2
Beñat Barbosa <binatbarzabal02@gmail.com>
2025-03-28 10:52:48 +0100

    html y css de el componente 2

--- src/app/components/conversation-textspace/conversation-textspace.component.ts
+++ src/app/components/conversation-textspace/conversation-textspace.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-conversation-textspace',
+  selector: 'chatbot-conversation-textspace',
   imports: [],
   templateUrl: './conversation-textspace.component.html',
+  styleUrl: './conversation-textspace.component.css',
 })
 export class ConversationTextspaceComponent { }
