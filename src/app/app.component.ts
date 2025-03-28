663ebdf14dc551ad52095fc2ed9550b524582470
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-03-28 10:20:26 +0100

    cambios simples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatbotNavegator } from "./components/navegator/navegator.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChatbotNavegator],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
