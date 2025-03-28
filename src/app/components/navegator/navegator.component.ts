f6cce0e7ebe3b488bfcf264f27ee2ffb09853645
Beñat Barbosa <binatbarzabal02@gmail.com>
2025-03-28 10:13:01 +0100

    hola

--- src/app/components/navegator/navegator.component.ts
+++ src/app/components/navegator/navegator.component.ts
@@ -1,13 +1,2 @@
-import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'chatbot-navegator',
-  templateUrl: 'name.component.html'
-})
-
-export class NameComponent implements OnInit {
-  constructor() { }
-
-  ngOnInit() { }
-}
 
