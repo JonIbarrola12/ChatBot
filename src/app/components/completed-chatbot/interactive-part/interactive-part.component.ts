7cc85f47f5c645cc1e42b92e7435f546555126da
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-03-31 12:21:52 +0200

    conexion con api

--- src/app/components/completed-chatbot/interactive-part/interactive-part.component.ts
+++ src/app/components/completed-chatbot/interactive-part/interactive-part.component.ts
@@ -9,13 +9,13 @@ import { ChatService } from '../../../services/chat.service';
   styleUrl: './interactive-part.component.css'
 })
 export class InteractivePartComponent {
-   public userMessage:string='';
-   public userAnswer:string='';
 
-   constructor(private chatService: ChatService){}
+  constructor(private chatService: ChatService){}
+
+
   sendMessage(pregunta: string){
     this.chatService.sendQuestion(pregunta).subscribe({
-    next: (response)=> this.userAnswer='Respuesta del Servidor: '+response
+    next: (response)=> console.log(response),
      })
   }
 
