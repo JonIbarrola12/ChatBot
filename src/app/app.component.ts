6df6b26ab37aa0b2f9c113f6badb512199eabee4
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-04-01 08:41:15 +0200

    Arreglao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChatbotNavegator } from './components/navegator/navegator.component';
+import { ChatbotNavegator } from './components/completed-chatbot/navegator/navegator.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChatbotNavegator],
+  imports: [RouterOutlet, ChatbotNavegator, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
