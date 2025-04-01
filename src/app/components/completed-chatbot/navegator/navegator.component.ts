1f5ac840a230bf2cb7f42531eb7aac8c7f268dc1
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-04-01 08:36:53 +0200

    Cambion de ruta

--- src/app/components/completed-chatbot/navegator/navegator.component.ts
+++ src/app/components/completed-chatbot/navegator/navegator.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { ChatService } from '../../services/chat.service';
+import { ChatService } from '../../../services/chat.service';
+
 
 @Component({
   selector: 'chatbot-navegator',
