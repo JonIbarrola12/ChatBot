4840efd9e25cc926438bf595e28d9cbc6ffd287d
Jon Ibarrola <jonibarrolalol12@gmail.com>
2025-04-03 11:34:28 +0200

    Pagina principal creada(faltan retoques pero es funcional)

--- src/app/components/init-button/init-button.component.ts
+++ src/app/components/init-button/init-button.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'chatbot-init-button',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './init-button.component.html',
   styleUrl: './init-button.component.css'
 })
 export class InitButtonComponent {
+  startChat(){
 
+  }
 }
